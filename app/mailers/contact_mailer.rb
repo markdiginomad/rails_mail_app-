7b10861f0a0faed43e2e95ab8435233aa7f0b44b
mark antony <markworld223@gmail.com>
2017-04-03 12:40:47 +0000

    modified, contatc controllers and mailers and deleted a few unecessary files

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    defaul to: 'markworld223@gmail.com'
+    default to: 'markworld223@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
